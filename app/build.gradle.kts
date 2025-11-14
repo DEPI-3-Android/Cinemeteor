b19d5df40322f800814bd729338db15d53270c2d
Ahmed Reda <235228@eru.edu.eg>
2025-11-14 19:02:54 +0200

    Add navigation bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/acms/cinemeteor/MainActivity.kt
+++ app/src/main/java/com/acms/cinemeteor/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.acms.cinemeteor.OnBoardingScreen.OnboardingScreen
 import com.acms.cinemeteor.ui.theme.CinemeteorTheme
+import com.example.compose.snippets.components.NavigationBarBottom
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
                         )
                     } else {
-                        MoviesHomeScreen()
+                        NavigationBarBottom(modifier = Modifier.padding(innerPadding))
                     }
 
 
